683a1433b1ca9f1179200e002d2f6c35134affe9
nayak001 <45924273+nayak001@users.noreply.github.com>
2021-07-27 17:04:06 +0530

    mis build 1.1.0

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,12 +2,12 @@ export const environment = {
   production: true,
   //baseUrl: 'http://localhost:1234/thinkzone/',          // Local
   //baseUrl: 'http://52.142.12.50:1234/thinkzone/',
-  baseUrl:'http://3.137.106.19:1234/thinkzone/', 
-  //baseUrl: 'http://3.135.181.145:1234/thinkzone/',        // Production
+  //baseUrl:'http://3.137.106.19:1234/thinkzone/',          // New Testing
+  baseUrl: 'http://3.135.181.145:1234/thinkzone/',        // Production
   ImageURL: 'http://3.135.181.145:1234/thinkzone/getimage/',
   uploadURL: 'http://3.135.181.145:1234/thinkzone/trainingcontentsuploads',
-  
+
   teacherappAuthkey: "key=AAAAQFKqh48:APA91bEOUG9D-S9bu4vYI1I_fT3fcwWh5tk1iMkzvQIp8SJM8WBy8hEB4i8ofqPmrc-GP0m9z11VAUpHwcs_70q-mamnn_nehlLWzeJltDwzwDbdV79H3wX8hXVBEA4uKXSOxRuv5n8a",
   managerappAuthkey: "",
-  version: '1.0.9'
+  version: '1.1.0'
 };
