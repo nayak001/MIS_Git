6c80fc9aced1c411c9b185987b0dadd0e2750596
Rajesh Senapati <pinkurajesh88@gmail.com>
2022-05-27 12:03:06 +0530

    newprod url added

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -5,7 +5,8 @@ export const environment = {
   //baseUrl: "http://3.135.181.145:1234/thinkzone/", // Production IP
 
   //baseUrl: "https://thinkzone.info/thinkzone/", // Test https
-  baseUrl: "https://thinkzone.org/thinkzone/", // Production https
+  // baseUrl: "https://thinkzone.org/thinkzone/", // Production https
+  baseUrl: "https://thinkzone.co/thinkzone/", // Production New
 
   ImageURL: "http://3.135.181.145:1234/thinkzone/getimage/",
   uploadURL: "http://3.135.181.145:1234/thinkzone/trainingcontentsuploads",
